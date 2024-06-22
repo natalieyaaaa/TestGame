d54cf5bc9801558cb64f4aedc6568d55dea8c511
natalieyaaaa <Yakovchuk.nataliia@trident-media.agency>
2024-06-22 16:25:07 +0300

    Initial commit

---
+++ TestGame/TestGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGameApp.swift
+//  TestGame
+//
+//  Created by Наташа Яковчук on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
